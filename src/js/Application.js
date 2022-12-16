b5f017b29e04167985a94d0fa6f8a3eb9f7a2c82
Kristiyan <javakris95@gmx.com>
2022-12-16 09:53:21 +0200

    changed js file

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import EventEmitter from "eventemitter3";
+import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -9,12 +10,78 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
+		
+		this._loading = document.querySelector('.progress')
 
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
-    });
+    //const box = document.createElement("div");
+    //box.classList.add("box");
+    //box.innerHTML = this._render({
+      //name: "Placeholder",
+     // terrain: "placeholder",
+     // population: 0,
+    //});
 
+    //document.body.querySelector(".main").appendChild(box);
+		
+		this._create()
+		
     this.emit(Application.events.READY);
+  }
+
+	async _load() {
+    let url = 'https://swapi.boom.dev/api/planets'
+    const result = []
+    do {
+      const response = await fetch(url)
+      const planets = await response.json()
+      url = planets.next;
+      result.push(...planets.results)
+    } while(url) 
+		return result
+	}
+
+	_create() {
+		this._load()
+			.then((planets) => planets.forEach(planet => {
+        
+        const div = document.createElement('div')
+        div.classList.add('box')
+        div.innerHTML = this._render({
+          name: planet.name,
+          terrain: planet.terrain,
+          population: planet.population,
+        })
+        this._stopLoading()
+        document.body.querySelector('.main').appendChild(div)
+			}));
+	}
+
+  _startLoading() {
+    this._loading.style.visibility = 'visible'
+  }
+
+  _stopLoading() {
+    this._loading.style.visibility = 'hidden'
+  }
+
+  _render({ name, terrain, population }) {
+    return `
+<article class="media">
+  <div class="media-left">
+    <figure class="image is-64x64">
+      <img src="${image}" alt="planet">
+    </figure>
+  </div>
+  <div class="media-content">
+    <div class="content">
+    <h4>${name}</h4>
+      <p>
+        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+        <br>
+      </p>
+    </div>
+  </div>
+</article>
+    `;
   }
 }
